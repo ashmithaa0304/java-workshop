3713f3485efcbf7eef3fa82f68b962b2ed93f99f
ashmithaa0304 <ashmithaa0304@gmail.com>
2025-07-02 14:55:27 +0530

    marks

---
+++ marks.java
@@ -0,0 +1,10 @@
+public class marks {
+    
+    public static void main(String[] args) {
+        int [] marks = {80,75,90,85,70};
+        for ( int mark :marks){
+            System.out.println(mark);
+        }
+
+    }
+}
