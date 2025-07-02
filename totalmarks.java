c2f5b2e07db2799c7f09ff1eea77cfe5770efe2e
ashmithaa0304 <ashmithaa0304@gmail.com>
2025-07-02 15:39:57 +0530

    totalmarks

---
+++ totalmarks.java
@@ -0,0 +1,11 @@
+public class totalmarks {
+    public static void main(String[] args) {
+        int [] marks = {80,75,90,85,70};
+        int total = 0;
+
+        for ( int mark :marks){
+            total += mark;
+        }
+        System.out.println("total marks:"+total);
+    }
+}
