bcbfc2f0d4a188f19a716d1996b0016716a9c4f8
ashmithaa0304 <ashmithaa0304@gmail.com>
2025-07-04 13:58:29 +0530

    Temperaturesensor

--- SRC/com/vetias/java/temperaturedata/beans/Temperaturesensor.java
+++ SRC/com/vetias/java/temperaturedata/beans/Temperaturesensor.java
@@ -1,6 +1,9 @@
 
 public class Temperaturesensor implements Sensor {
     private float temperature;
+    public static void main(String[] args) {
+        
+    }
     
     public Temperaturesensor(float atemperature)
      {
