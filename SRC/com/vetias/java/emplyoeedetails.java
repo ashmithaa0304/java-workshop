aeb0d57e8bee349db5518682317bf711f0a02ee5
ashmithaa0304 <ashmithaa0304@gmail.com>
2025-07-04 10:39:44 +0530

    employeedetails

---
+++ SRC/com/vetias/java/emplyoeedetails.java
@@ -0,0 +1,22 @@
+public class emplyoeedetails {
+    public class EmplyoeesDetailsArray {
+   public static void main(String[] args) {
+      String[][] employees=new String[5][2];
+      employees[0][0] = "40000";
+      employees[0][1] = "nivi";
+      employees[1][0] = "35000";
+      employees[1][1] = "ashmi";
+      employees[2][0] = "45000";
+      employees[2][1] = "nithi";
+      employees[3][0] = "40000";
+      employees[3][1] = "john";
+      employees[4][0] = "50000";
+      employees[4][1] = "deva";
+      System.out.println("Employeedetails:");
+      for (int i = 0; i < employees.length; i++) {
+        System.out.println("salary:" + employees[i][0] + ",Name: " + employees[i][1]);
+      }
+   }
+}   
+
+}
--- SRC/com/vetias/java/temperaturedata/beans/Floor.java
+++ SRC/com/vetias/java/temperaturedata/beans/Floor.java
@@ -1,5 +1,7 @@
 package SRC.com.vetias.java.temperaturedata.beans;
 
 public class Floor {
+    private String name;
+    private int number;
     
 }
