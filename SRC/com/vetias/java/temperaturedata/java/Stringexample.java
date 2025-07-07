1d359eb84c7992f7d75827c5604d84390741d6eb
ashmithaa0304 <ashmithaa0304@gmail.com>
2025-07-07 10:50:53 +0530

    Stringexample

---
+++ SRC/com/vetias/java/temperaturedata/java/Stringexample.java
@@ -0,0 +1,14 @@
+public class Stringexample {
+   public static void main(String[] args) {
+      String firstname = "Raja";
+      String shortname = "Raja";
+      String buddyname = new String ( "Raja");
+      System.out.println(firstname == shortname);
+      System.out.println(firstname == buddyname); 
+      
+
+      String sentence = "this program shows how can we split a string into multiple string";
+      String words[] = sentence.split(" "); 
+      System.out.println(words.length);
+}
+}
