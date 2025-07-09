31ad259db14eaf43801a2f42f6c100afa8700650
ashmithaa0304 <ashmithaa0304@gmail.com>
2025-07-09 11:40:21 +0530

    CharacterBasedReaderExample

---
+++ SRC/com/vetias/java/StandardLibs/FileCopyUsingBinary.java
@@ -0,0 +1,7 @@
+public class FileCopyUsingBinary {
+public static void main(String[] args) {
+    try(FileInputStream inputstream=new FileInputStream( ""));
+    FileOutputStream OutStream=new FileOutputStream( "");
+}
+
+}
